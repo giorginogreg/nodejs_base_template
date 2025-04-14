f5484393012bf4aa883301104f58626a5f293e6c
Gregorio Giorgino <giorgino.greg@gmail.com>
2025-04-14 09:00:24 +0200

    chore: extracted const for API prefix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,23 +2,22 @@ import express, { NextFunction, Request, Response } from 'express';
 //import type { Request, Response } from 'express';
 import usersRouter from './apps/users/entry-points/api/v1/users-router';
 
+const API_PREFIX = '/api/v1';
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-
-app.use('/api/v1/users', usersRouter);
+app.use(`${API_PREFIX}/users`, usersRouter);
 
 // Below route is trigerred when any error is is thrown
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).json({ message: err.message });
+	console.error(err);
+	res.status(500).json({ message: err.message });
 });
 
 // Avvio del server
 app.listen(port, () => {
-  console.log(`🚀 Server avviato con successo sulla porta ${port}`);
-  console.log(`📝 Endpoint principale: http://localhost:${port}`);
-  console.log(`🔍 Endpoint di test: http://localhost:${port}/api/test`);
+	console.log(`🚀 Server avviato con successo sulla porta ${port}`);
+	console.log(`📝 Endpoint principale: http://localhost:${port}${API_PREFIX}/users`);
 });
 
 export default app;
