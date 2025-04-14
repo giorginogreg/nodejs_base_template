4c70666692b01f0991a15f84cac3e3b2971dd199
Gregorio Giorgino <giorgino.greg@gmail.com>
2025-04-14 14:35:57 +0200

    fix: improved syntax for tests

--- backend/tests/sum.test.ts
+++ backend/tests/sum.test.ts
@@ -1,5 +1,7 @@
 const sum = require('#src/libraries/sum/sum.js');
-
-test('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
+describe('sum', () => {
+	it('should add 1 + 2 to equal 3', () => {
+		const result = sum(1, 2);
+		expect(result).toBe(3);
+	});
 });
