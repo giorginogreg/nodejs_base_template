0cdea85feae5460c26928317bfdd8bfdbc46fba4
Gregorio Giorgino <giorgino.greg@gmail.com>
2025-04-14 12:32:58 +0200

    chore: added code statuses for responses

--- backend/src/apps/users/entry-points/api/v1/users-controller.ts
+++ backend/src/apps/users/entry-points/api/v1/users-controller.ts
@@ -3,38 +3,39 @@ import { UserService } from '../../../domain/user-service';
 import { RequestHandler } from 'express';
 //import UserService from '../../../domain/user-service';
 import type { Request, Response } from 'express';
+import { ResponseCodes } from 'http-constants-ts';
 const userService = new UserService(new PrismaUserRepository());
 
 export const getAllUsers: RequestHandler = async (req: Request, res: Response) => {
-  const users = await userService.getAllUsers();
-  res.status(200).json({ message: 'Users fetched successfully', data: users });
+	const users = await userService.getAllUsers();
+	res.status(ResponseCodes.OK).json({ message: 'Users fetched successfully', data: users });
 };
 
 export const getUserById: RequestHandler = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const user = await userService.getUserById(parseInt(id));
-  if (!user) {
-    res.status(404).json({ error: 'User not found' });
-    return;
-  }
-  res.status(200).json({ message: `User fetched successfully`, data: user });
+	const { id } = req.params;
+	const user = await userService.getUserById(parseInt(id));
+	if (!user) {
+		res.status(ResponseCodes.NOT_FOUND).json({ error: 'User not found' });
+		return;
+	}
+	res.status(ResponseCodes.OK).json({ message: `User fetched successfully`, data: user });
 };
 
 export const createUser: RequestHandler = async (req: Request, res: Response) => {
-  const userData = req.body;
-  const newUser = await userService.createUser(userData);
-  res.status(201).json({ message: 'User created', data: newUser });
+	const userData = req.body;
+	const newUser = await userService.createUser(userData);
+	res.status(ResponseCodes.CREATED).json({ message: 'User created', data: newUser });
 };
 
 export const updateUser: RequestHandler = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const userData = req.body;
-  const updatedUser = await userService.updateUser(parseInt(id), userData);
-  res.status(200).json({ message: `User ${id} updated`, data: updatedUser });
+	const { id } = req.params;
+	const userData = req.body;
+	const updatedUser = await userService.updateUser(parseInt(id), userData);
+	res.status(ResponseCodes.OK).json({ message: `User ${id} updated`, data: updatedUser });
 };
 
 export const deleteUser: RequestHandler = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const deletedUser = await userService.deleteUser(parseInt(id));
-  res.status(200).json({ message: `User ${id} deleted`, data: deletedUser });
+	const { id } = req.params;
+	const deletedUser = await userService.deleteUser(parseInt(id));
+	res.status(ResponseCodes.OK).json({ message: `User ${id} deleted`, data: deletedUser });
 };
