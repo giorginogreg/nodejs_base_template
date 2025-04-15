06f22eb9eeb32ca75ddb2f9574d1f63b15d4e3ff
Gregorio Giorgino <giorgino.greg@gmail.com>
2025-04-15 11:05:53 +0200

    chore: removed ending semicolons

--- backend/src/createApp.ts
+++ backend/src/createApp.ts
@@ -1,21 +1,21 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express'
 //import type { Request, Response } from 'express';
-import { ResponseCodes } from 'http-constants-ts';
+import { ResponseCodes } from 'http-constants-ts'
 
-import usersRouter from './apps/users/entry-points/api/v1/users-router';
+import usersRouter from './apps/users/entry-points/api/v1/users-router'
 
 export function createApp() {
-	const app = express();
+	const app = express()
 
-	const API_PREFIX = '/api/v1';
+	const API_PREFIX = '/api/v1'
 
-	app.use(express.json());
-	app.use(`${API_PREFIX}/users`, usersRouter);
+	app.use(express.json())
+	app.use(`${API_PREFIX}/users`, usersRouter)
 
 	// Below route is trigerred when any error is is thrown
 	app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-		console.error(err);
-		res.status(ResponseCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
-	});
-	return app;
+		console.error(err)
+		res.status(ResponseCodes.INTERNAL_SERVER_ERROR).json({ message: err.message })
+	})
+	return app
 }
